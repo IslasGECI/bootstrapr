da24d04d93ff3872db4f8642787a301b07517bf8
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-03-23 15:47:02 -0700

    🧪 Test for `make_table` and `make_table_without_round`

---
+++ tests/testthat/test-make_table_function.R
@@ -0,0 +1,17 @@
+describe("make_table()", {
+  it("check round", {
+    data <- read_csv("../data/densidad_madrigueras_mergulo_morro.csv", show_col_types = FALSE)
+    obtained <- make_table(data)
+    expected <- read_csv("../data/intervalo_densidad_mergulo_morro.csv", col_types = c("cnnn"), show_col_types = FALSE)
+    expect_equal(obtained, expected, ignore_attr = TRUE)
+  })
+})
+
+describe("make_table_without_round()", {
+  it("does not round", {
+    data <- read_csv("../data/densidad_madrigueras_mergulo_morro.csv", show_col_types = FALSE)
+    obtained <- make_table_without_round(data)
+    expected <- read_csv("../data/intervalo_densidad_mergulo_morro_without_round.csv", col_types = c("cnnn"), show_col_types = FALSE)
+    expect_equal(obtained, expected, ignore_attr = TRUE, tolerance = 1e-2)
+  })
+})
