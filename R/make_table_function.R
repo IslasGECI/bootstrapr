12e2ddecb345796694c6c10bcc394b0b3067b92b
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-03-23 16:00:35 -0700

    ♻️ Extract methods

--- R/make_table_function.R
+++ R/make_table_function.R
@@ -2,29 +2,34 @@ library(tidyverse)
 
 make_table <- function(data) {
   temporada <- names(data)
-  ntemporadas <- length(temporada)
-  table <- make_cuantiles(data, 1)
+  table <- .setup_table(data)
   round_number <- 2
-  for (i in 2:ntemporadas) {
-    quantil <- make_cuantiles(data, i)
-    table <- rbind(table, quantil)
-  }
-  table <- table %>%
-    mutate("minimo" = round(q3 - q1, round_number), "maximo" = round(q5 - q3, round_number), "central" = round(q3, round_number)) %>%
+  table <- .add_min_max_central_columns(table) %>%
     cbind(temporada) %>%
     select(c("temporada", "central", "minimo", "maximo"))
   return(table)
 }
 
+.add_min_max_central_columns <- function(table) {
+  round_number <- 2
+  table <- table %>%
+    mutate("minimo" = round(q3 - q1, round_number), "maximo" = round(q5 - q3, round_number), "central" = round(q3, round_number))
+  return(table)
+}
 
-make_table_without_round <- function(data) {
-  temporada <- names(data)
-  ntemporadas <- length(temporada)
+.setup_table <- function(data) {
+  ntemporadas <- ncol(data)
   table <- make_cuantiles(data, 1)
   for (i in 2:ntemporadas) {
     quantil <- make_cuantiles(data, i)
     table <- rbind(table, quantil)
   }
+  return(table)
+}
+
+make_table_without_round <- function(data) {
+  temporada <- names(data)
+  table <- .setup_table(data)
   table <- table %>%
     mutate("minimo" = q3 - q1, "maximo" = q5 - q3, "central" = q3) %>%
     cbind(temporada) %>%
