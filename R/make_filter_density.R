a80e59cf9915cc418f495cd0c414cba348aa8abd
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-22 21:10:38 +0000

    🚸 add documentation to exported functions

--- R/make_filter_density.R
+++ R/make_filter_density.R
@@ -1,3 +1,12 @@
+#' Calcular densidad de madrigueras
+#'
+#' Calcula la densidad de madrigueras con actividad aparente por unidad de area.
+#'
+#' @param data Un data frame que contiene las columnas `Madrigueras_con_actividad_aparente`,
+#'   `Area_del_cuadrante` y `Temporada`.
+#'
+#' @return Un data frame con las columnas `Temporada` y `densidad`.
+#'
 #' @export
 make_filter_density <- function(data) {
   data <- data |>
